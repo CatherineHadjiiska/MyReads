9c4e9c5c90844f3e1af13b4c4414f0fc99aeef0d
Catherine Hadjiiska <catherine.hadjiiska@gmail.com>
2018-08-07 10:57:27 +0300

    ERROR: A <Router> may have only one child element

--- src/Book.js
+++ src/Book.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 
 
 class Book extends Component {
+
 	render () {
-		let displayThumbnail = this.props.book.imageLinks ?
-		this.props.book.imageLinks.thumbnail : 
-		'';
+		const book = this.props; 
 
 		return (
 			<div className="book">
 			<div className="book-top">
-			<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${displayThumbnail}"`}}></div>
+			<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.image.thumbnail}"`}}></div>
 			<div className="book-shelf-changer">
 			<select
 			onChange={(event) => this.props.moveShelf(
